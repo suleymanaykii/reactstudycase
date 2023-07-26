0c0a15f8367221f342555b04273a6ba859247181
Aykı Ailesi <kanalimsuleyman@gmail.com>
2023-07-26 23:27:04 +0300

    Button click random number

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [quotes, setQuotes] = useState([]);
+    let [setRandomQuote] = useState("");
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch("https://type.fit/api/quotes");
+            const data = await response.json();
+            setQuotes(data);
+            let randIndex = Math.floor(Math.random() * data.length);
+            console.log(randIndex);
+        }
+
+        fetchData();
+    }, []);
+
+    const getNewQuote = () => {
+
+        let randIndex = Math.floor(Math.random() * quotes.length);
+        let randColorIndex = Math.floor(Math.random());
+        console.log('randIndex');
+        console.log(quotes[randIndex]);
+    };
+
+    return (
+        <div>
+            <h1>Random Quotes</h1>
+            <ul>
+                {quotes.map(({ author, text }, index) => (
+                    <li key={index}>
+                        "{text}" ~{author}
+                    </li>
+                ))}
+            </ul>
+            <button onClick={getNewQuote}>Click For Random Quotes</button>
+        </div>
+    );
 }
-
-export default App;
