849f447ca75f1dbd438745f5fb667334c1bcc05b
Aykı Ailesi <kanalimsuleyman@gmail.com>
2023-07-29 01:30:29 +0300

    Button click random number

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,21 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function App() {
-    const [quotes, setQuotes] = useState([]);
-    let [setRandomQuote] = useState("");
+    let [randomQuote, setRandomQuote] = useState("");
 
-    useEffect(() => {
+    const getNewQuote = () => {
         async function fetchData() {
             const response = await fetch("https://type.fit/api/quotes");
             const data = await response.json();
-            setQuotes(data);
             let randIndex = Math.floor(Math.random() * data.length);
-            console.log(randIndex);
+            setRandomQuote(data[randIndex].text)
         }
-
         fetchData();
-    }, []);
-
-    const getNewQuote = () => {
-
-        let randIndex = Math.floor(Math.random() * quotes.length);
-        let randColorIndex = Math.floor(Math.random());
-        console.log('randIndex');
-        console.log(quotes[randIndex]);
     };
 
     return (
         <div>
-            <h1>Random Quotes</h1>
-            <ul>
-                {quotes.map(({ author, text }, index) => (
-                    <li key={index}>
-                        "{text}" ~{author}
-                    </li>
-                ))}
-            </ul>
+            <h1>Random Quotes {randomQuote}</h1>
             <button onClick={getNewQuote}>Click For Random Quotes</button>
         </div>
     );
