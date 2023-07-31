0aaf37829341583bcdf090339594f81003cafb71
Mustafa YILDIZ <mustafayildiz.m@gmail.com>
2023-07-31 20:30:51 +0300

    test

---
+++ src/menu-components/Navigation.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Navigation from './Navigation';
+
+describe('Navigation', () => {
+    test('renders home, categories, and search links', () => {
+        render(
+            <Router>
+                <Navigation />
+            </Router>
+        );
+
+        expect(screen.getByRole('link', { name: /home/i })).toBeVisible();
+        expect(screen.getByRole('link', { name: /categories/i })).toBeVisible();
+        expect(screen.getByRole('link', { name: /search/i })).toBeVisible();
+    });
+
+    test('toggles the menu when clicked', () => {
+        render(
+            <Router>
+                <Navigation />
+            </Router>
+        );
+
+
+        const toggleButton = screen.getByRole('button', { name: /toggle navigation/i });
+        fireEvent.click(toggleButton);
+
+        expect(screen.getByText('Home')).toBeVisible();
+        expect(screen.getByText('Categories')).toBeVisible();
+        expect(screen.getByText('Search')).toBeVisible();
+    });
+});
